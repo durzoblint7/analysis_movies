53a8fc23a371e7d63b9f4037833b18db7bd43342
durzoblint7 <78142723+durzoblint7@users.noreply.github.com>
2021-04-03 04:22:12 +0200

    Add files via upload

---
+++ movies_data/install_libraries.sh
@@ -0,0 +1,15 @@
+pip install pandas
+pip install numpy
+pip install matplotlib
+pip install sklearn
+pip install IPython
+pip install ipywidgets
+pip install math
+pip install seaborn
+pip install scipy
+pip install IPython
+pip install jupyterlab
+
+jupyter nbextension enable --py widgetsnbextension
+
+jupyter notebook analysis-movies.ipynb
